b8bdc9b721d7309bc164429df38c13114f8120de
Sonia Quintiao <soniaquintiao@gmail.com>
2019-12-11 09:40:59 +0000

    Update code to enable: - temperature toggling - temperature reset - power saving on-of - change in temperature color based on usage

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temp);
+  updateTemp();
+
+  $('#temperature-up').click(function() { // event listner
+    thermostat.up(); // update model
+    updateTemp(); // update view
+  });
+
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    updateTemp();
+  });
+
+  $('#temperature-reset').click(function() {
+    thermostat.reset();
+    updateTemp();
+  });
+
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#ps-status').text('ON')
+    updateTemp();
+  });
+
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#ps-status').text('OFF')
+    updateTemp();
+  })
+
+  // need to add feature in model such that when psm turned back on,
+  // temp is reset to 20. At the moment, if you turn it back on whilst above 25
+  // temp can be raised infinitely.
+
+  function updateTemp() { //refactored
+    $('#temperature').text(thermostat.temp);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  };
 });
